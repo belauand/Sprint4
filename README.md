0f07a60417ccc06bb04df857a2cd4f6c734e9dcf
belauand <belauand@hotmail.com>
2026-06-22 22:04:26 -0300

    Readme

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Sprint4
 Projeto MBA Sprint 4
+Dashboards sobre venda de veículos
--- app.py
+++ app.py
@@ -5,7 +5,8 @@ import streamlit as st
 st.header('Pesquisa de vendas de carros')  # título da aplicação
 
 car_data = pd.read_csv('vehicles_us.csv')  # lendo os dados
-hist_button = st.button('Criar histograma')  # criar um botão
+hist_button = st.button('Criar histograma')  # criar um botão hitograma
+disp_button = st.button('Criar gráfico dedispersão')  # criar um grafico de dispersão
 
 if hist_button:  # se o botão for clicado
     # escrever uma mensagem
@@ -14,6 +15,18 @@ if hist_button:  # se o botão for clicado
 
     # criar um histograma
     fig = px.histogram(car_data, x="odometer")
+
+    # exibir um gráfico Plotly interativo
+    st.plotly_chart(fig, use_container_width=True)
+
+
+if disp_button:  # se o botão for clicado
+    # escrever uma mensagem
+    st.write(
+        'Criando um gráfico de dispersão para o conjunto de dados de anúncios de vendas de carros')
+
+    # criar um gráfico de dispersão
+    fig = px.scatter(car_data, x="odometer", y="price")
 
     # exibir um gráfico Plotly interativo
     st.plotly_chart(fig, use_container_width=True)
